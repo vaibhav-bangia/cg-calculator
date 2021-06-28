b32b2eb6e2173cd3d0688b37b5569fee9e25ef05
Vaibhav Bangia <39953637+vaibhav-bangia@users.noreply.github.com>
2021-06-28 21:08:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function testResults2(form) {
+    var credits = [20,20,22,22,22]
+    var result = 0 ;
+    var sum = 0 ; 
+    for( var i=0;i<form.inputbox.length;i++){
+        var cg =  form.inputbox[i].value ; 
+        console.log(cg) ;
+        result+= credits[i]*cg;
+        sum+= parseFloat( cg) ;
+    }
+    console.log(sum)
+    alert("Aggregate CG: " + result/106);
+}
+
+
 function testResults(form) {
     var credits = [20,20,22,22,22]
     var result = 0 ;
