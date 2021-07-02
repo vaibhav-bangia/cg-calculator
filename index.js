fd74fca406525d50461dc6eedffb424ce682e229
Vaibhav Bangia <vaibhavbangia@gmail.com>
2021-07-02 22:34:29 +0530

    added code for all 8 sems

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 function testResults(form) {
-    var credits = [20,20,22,22,22]
+    var credits = [20,20,22,22,22,22,22,22]
     var result = 0 ;
     var sum = 0 ; 
+    var total_credits = 0 ;
     for( var i=0;i<form.inputbox.length;i++){
         var cg =  form.inputbox[i].value ; 
-        console.log(cg) ;
+            if(cg.length==0){
+                cg=0;
+                //total_credits-=credits[i];
+            }
+            else if(cg==0){
+                console.log('0 cpga wtf'); 
+            }
+            else{
+                total_credits+=credits[i];
+                console.log(cg);
+            }
         result+= credits[i]*cg;
         sum+= parseFloat( cg) ;
     }
-    console.log(sum)
-    alert("Aggregate CG: " + result/106);
+    
+    console.log(total_credits)
+    alert("Aggregate CG: " + result/total_credits);
 }
 
